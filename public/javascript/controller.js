a4860b19114a7844fe9499f2c983bbc7e2627650
rkpattnaik780 <rkpattnaik780@gmail.com>
2017-09-09 12:02:07 +0530

    Angular removed and favicon fixed for arcgis

--- public/javascript/controller.js
+++ public/javascript/controller.js
@@ -1,22 +1,49 @@
-"use strict";
+$(document).ready(function () {
+    require([
+        "esri/Map",
+        "esri/views/SceneView",
+        "dojo/domReady!"
+    ], function (Map, SceneView) {
+        // Code to create the map and view will go here
 
-var app = angular.module("myApp", ["ngRoute"]);
+        require([
+            "esri/Map",
+            "esri/views/SceneView",
+            "dojo/domReady!"
+        ], function (Map, SceneView) {
 
-app.config(function ($routeProvider, $locationProvider) {
+            var map = new Map({
+                basemap: "hybrid",
+                ground: "world-elevation"
+            });
 
-    $routeProvider
-        // route for the home page
+            var view = new SceneView({
+                container: "viewDiv",
+                map: map,
+                scale: 10000,
+                 /*  //  center: [-101.17, 21.78]
+                   //  center : [20.95,85.09]
+                     center : [85.09,20.95],
+                     zoom : 13 */
+            });
 
-        .when('/', {
-            templateUrl: '../pages/home.html',
-            controller: 'mainController'
+            view
+                .then(function () {
+                    // SceneView is now ready for display and can be used. Here we will
+                    // use goTo to view a particular location at a given zoom level, camera
+                    // heading and tilt.
+                    view.goTo({
+                        center: [85.7758, 20.2764],
+                        zoom: 20,
+                        heading: 30,
+                        tilt: 0
+                    })
+                })
+                .otherwise(function (err) {
+                    // A rejected view indicates a fatal error making it unable to display,
+                    // this usually means that WebGL is not available, or too old.
+                    console.error("SceneView rejected:", err);
+                });
         });
-
-    $locationProvider.html5Mode(true);
-});
-
-app.controller('mainController', function ($scope , $http) {
-      $http.get("https://api.github.com/users/rkpattnaik780").then(function(res){
-            console.log(res.data);
-      });
+    });
 });
